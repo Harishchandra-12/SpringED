698e20fae71676840078e57de7069c7712cd2510
Harishchandra-12 <harishpenumati12@gmail.com>
2025-07-22 10:52:26 -0500

    Set Up Done

---
+++ src/main/java/com/example/SpringED/SpringEdApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringED;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEdApplication.class, args);
+	}
+
+}
